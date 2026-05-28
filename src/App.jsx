47b7b572eb12a67972b7b1796abbaaf10b13a20f
antsa <antsa@MacBook-Pro-de-antsa.local>
2026-05-28 17:21:08 -0400

    feat(data): nouvelle liste joueurs Groupe A & B (maj noms fichiers)

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ import MatchPage from './pages/MatchPage';
 import { GSHEET_HORAIRE_URL } from './services/adminService';
 
 const EXCEL_HORAIRE_FALLBACK = '/data/HORAIRE_2026.xlsx';
-const EXCEL_PLAYERS_A_URL    = '/data/Copie de @LISTE  GROUPE A .xlsx';
-const EXCEL_PLAYERS_B_URL    = '/data/Copie de Liste GROUPE B.xlsx';
+const EXCEL_PLAYERS_A_URL    = '/data/@LISTE  GROUPE A .xlsx';
+const EXCEL_PLAYERS_B_URL    = '/data/@Liste GROUPE B.xlsx';
 
 function DataLoader({ children }) {
   const { loadHoraire, loadPlayers } = useLeagueData();
