7159e3bf42bc3b53dea46263369c84eeb626676b
antsa <antsa@MacBook-Pro-de-antsa.local>
2026-07-14 23:32:02 -0400

    fix(players): nouvelles listes Groupe A + B (sans suffixe)

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ import MatchPage from './pages/MatchPage';
 import { GSHEET_HORAIRE_URL } from './services/adminService';
 
 const EXCEL_HORAIRE_FALLBACK = '/data/HORAIRE_2026.xlsx';
-const EXCEL_PLAYERS_A_URL    = '/data/@LISTE  GROUPE A -7.xlsx';
-const EXCEL_PLAYERS_B_URL    = '/data/@Liste GROUPE B-5.xlsx';
+const EXCEL_PLAYERS_A_URL    = '/data/@LISTE  GROUPE A.xlsx';
+const EXCEL_PLAYERS_B_URL    = '/data/@Liste GROUPE B.xlsx';
 
 function DataLoader({ children }) {
   const { loadHoraire, loadPlayers } = useLeagueData();
